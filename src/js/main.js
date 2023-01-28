711c925f1fcc20b3a658429fd5624c386ba7f826
lisamessier <lisa.messier@outlook.com>
2023-01-28 17:32:35 -0500

    Add rotating name

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,21 @@ import {initBootstrap} from "./bootstrap.js";
 
 // Loading bootstrap with optional features
 initBootstrap({
-  tooltip: true,
-  popover: true,
-  toasts: true,
+  tooltip: false,
+  popover: false,
+  toasts: false,
 });
 
 // Your app code
 console.log(`Hello ${process.env.HELLO}`);
+let word = ["cyclone", "curling", "carlos", "calf", "crime"]
+let title = document.getElementById("sccsheader")
+let i = 0
+
+var intervalID = window.setInterval(() => {
+  title.textContent = `Swarthmore College ${word[i]} Society`
+  i++
+  if (i==word.length) {
+    i=0
+  }
+}, 1000)
